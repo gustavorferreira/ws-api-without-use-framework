204ff8a05df85f7c1d24d84b721fefe3d1cf2c0c
4233532 - Gustavo R. Ferreira <gustavor.ferreira@se.df.gov.br>
2021-12-06 20:15:04 -0300

    [ADD] - documentation of how use system. Part 7.

--- source/App/Config/core_cfg.php
+++ source/App/Config/core_cfg.php
@@ -10,7 +10,6 @@ const DATA_LAYER_CONFIG = [
     "username" => "",
     "passwd" => "",
     "options" => [
-        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
         PDO::ATTR_CASE => PDO::CASE_NATURAL
